040ec6d5011bed252b9628f440d0bdbc336cf46b
Jacob Lorenz <jacob.lorenz@b-tu.de>
2018-04-26 00:34:19 +0200

    Impl: Update
    + improved section on data durable structures
        * improved intro
        * moved bit on volatile data structures to MVCC
        * improved section structure
    + made replaced utils with data structures in arch figure
    + improved script for finding acronyms
    + cosmetics

--- print_ac.sh
+++ print_ac.sh
@@ -3,5 +3,5 @@ if [ $# -eq 0 ]
     echo "No arguments supplied"
     exit
 fi
-grep -Rho "[A-Z]\{2,5\}" $1 | sort | uniq
+grep -Rho "\b[A-Z]\{2,5\}\b" $1 | sort | uniq
 # grep -Rho "[A-Z]\{2,5\}" contents/kvs/*.tex | sort | uniq
